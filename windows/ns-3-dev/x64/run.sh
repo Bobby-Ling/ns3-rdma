9e6e29a08724f33691c06343b5c6f003bd111229
Bobby-Ling <bobby-ling@outlook.com>
2024-11-24 11:50:42 +0800

    更新测试

--- windows/ns-3-dev/x64/run.sh
+++ windows/ns-3-dev/x64/run.sh
@@ -6,3 +6,5 @@
 
 NS_LOG="*=logic" ./main.exe mix/small/config.txt
 python mix/gen_result.py --config  mix/small/config.txt --trace mix/small/mix.tr
+../../../main-v4.exe config.txt | tee out.txt
+python ../../gen_result.py && python ../../plot_generator.py && cat ../origin/plot-result.txt
